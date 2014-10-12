d8e90ef75d7351464b3bda5b0c64a77544e1bb6a
hronos <dlennart@daniels-mbp-2.home>
2014-10-12 07:40:17 +0100

    addAvatar method added

--- src/main/java/uk/ac/dundee/computing/aec/instagrim/lib/Keyspaces.java
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/lib/Keyspaces.java
@@ -47,7 +47,8 @@ public final class Keyspaces {
                     + "      first_name text,\n"
                     + "      last_name text,\n"
                     + "      email set<text>,\n"
-                    + "      addresses  map<text, frozen <address>>\n"
+                    + "      addresses  map<text, frozen <address>>,\n"
+                    + "      avatar  blob"
                     + "  );";
             Session session = c.connect();
             try {
